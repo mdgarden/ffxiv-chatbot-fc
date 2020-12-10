5069d45d50769fd5bb9c6e2e3030e5c222639e80
mdgarden <chooi9522@gmail.com>
2020-12-11 07:19:47 +0900

    edit main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     line_bot_api.reply_message(
-        event.reply_token, TextSendMessage(text=event.message.text)
+        event.reply_token, TextSendMessage(text="test")
     )
 
 
