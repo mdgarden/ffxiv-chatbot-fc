68ca6767105e98a45d06e07c08e5611915853064
mdgarden <chooi9522@gmail.com>
2020-12-11 07:19:47 +0900

    edit main.py

--- config.py
+++ config.py
@@ -1,3 +1,10 @@
+
+# LINE ACCOUNT INFO
 CHANNEL_ACCESS_TOKEN = "shgb21yMVHRTiQDTmbkzacpe+TlicRY7ghVUOqeiqDIwK2At3I6TsGsT0L5+uNj5VTN9YjoJpQKzswRAkMivi43Hb/YayHPS2xzBate0Cnzq8bz49tJufImQcWyIuO9cwhrOWOdfDjTzMi1tcTbpdgdB04t89/1O/w1cDnyilFU="
 CHANNEL_SECRET = "e234b3488f345d1d3bf63f6086ae4284"
 CHANNEL_ID = "1654605587"
+
+# GOOGLE DRIVE API
+DRIVE_SECRET_JSON = 'fc-chatbot-298206-0f1e9c108c8b.json'
+DB_SHEET = 'https://docs.google.com/spreadsheets/d/13PHjRMsNZDmLISZeglFW7sqSuWHnc9UXf4Bndwu5HTY/edit#gid=0'
+
--- gsheet.py
+++ gsheet.py
@@ -1,16 +1,17 @@
 import gspread
+import config
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = [
 'https://spreadsheets.google.com/feeds',
 'https://www.googleapis.com/auth/drive',
 ]
-json_file_name = 'fc-chatbot-298206-0f1e9c108c8b.json'
+json_file_name = config.DRIVE_SECRET_JSON
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
 gc = gspread.authorize(credentials)
 
-spreadsheet_url = 'https://docs.google.com/spreadsheets/d/13PHjRMsNZDmLISZeglFW7sqSuWHnc9UXf4Bndwu5HTY/edit#gid=0'
+spreadsheet_url = config.DB_SHEET
 
 # 스프레드 시트 문서 가져오기 
 doc = gc.open_by_url(spreadsheet_url)
