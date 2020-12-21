6c9dcb71efac605b485f7e6e8d8b007b551ec258
mdgarden <chooi9522@gmail.com>
2020-12-21 20:40:42 +0900

    201221

--- gsheet.py
+++ gsheet.py
@@ -3,8 +3,8 @@ import config
 from oauth2client.service_account import ServiceAccountCredentials
 
 scope = [
-'https://spreadsheets.google.com/feeds',
-'https://www.googleapis.com/auth/drive',
+    "https://spreadsheets.google.com/feeds",
+    "https://www.googleapis.com/auth/drive",
 ]
 json_file_name = config.DRIVE_SECRET_JSON
 
@@ -13,11 +13,13 @@ gc = gspread.authorize(credentials)
 
 spreadsheet_url = config.DB_SHEET
 
-# 스프레드 시트 문서 가져오기 
+# 스프레드 시트 문서 가져오기
 doc = gc.open_by_url(spreadsheet_url)
 
 # 시트 선택하기
-worksheet = doc.worksheet('시트1')
+worksheet = doc.worksheet("시트1")
 
-cell_data = worksheet.acell('B3').value
+cell_data = worksheet.acell("B3").value
 print(cell_data)
+
+# 명언록
