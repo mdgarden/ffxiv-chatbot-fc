c60fd49adf962c6a0f0ab1143a7b0b94f3e7ae23
mdgarden <chooi9522@gmail.com>
2021-02-06 13:53:15 +0900

    update command and link

--- command.py
+++ command.py
@@ -37,10 +37,10 @@ def find_command(command):
 
 
 def send_manual():
-    message = "명령어 목록이에용!\n@+캐릭터 이름 : 로드스톤 링크\n@점검 : 최신 점검 관련 공지 목록(한섭 업뎃 예정)\n!+검색어 : 해당 아이템을 검색 후 언어별 이름 출력(업뎃예정)\n@공홈 : 공홈 링크를 불러옵니다"
+    message = "명령어 목록이에용!\n@+캐릭터 이름 : 로드스톤 링크\n@점검 : 최신 점검 관련 공지 목록(한섭 업뎃 예정)\n!+검색어 : 해당 아이템을 검색 후 언어별 이름 출력\n@링크 : 각종 링크를 불러옵니다"
     return message
 
 
 def send_link():
-    message = "글섭 공홈 : https://jp.finalfantasyxiv.com/lodestone/ \n한섭 공홈 : https://www.ff14.co.kr/main \n지름신 강림 : https://store.jp.square-enix.com/item_list.html?sale_cd=1#SERIES=11&pointercat=SERIES"
+    message = "글섭 공홈 : https://jp.finalfantasyxiv.com/lodestone/ \n한섭 공홈 : https://www.ff14.co.kr/main \n지름신 : https://store.jp.square-enix.com/item_list.html?sale_cd=1#SERIES=11&pointercat=SERIES"
     return message
