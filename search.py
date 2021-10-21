7bcf4362be8fe62ea9820cd8703cc9d7e78b8586
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-10-21 21:16:03 +0900

    Formatter/Linter Flake8 적용

--- search.py
+++ search.py
@@ -1,16 +1,9 @@
-import time
 import os
 import requests
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.utils import ChromeType
-from chromedriver_py import binary_path
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36"
@@ -21,6 +14,17 @@ FFXIV_LS_DB = "/lodestone/playguide/db/search/?q="
 FFXIV_JP_URL = "https://jp.finalfantasyxiv.com"
 FFXIV_NA_URL = "https://na.finalfantasyxiv.com"
 
+hangul = re.compile("[^ \u3131-\u3163\uac00-\ud7a3]+")  # 한글만 추출하는 정규식
+
+item_search_result = {
+    "kr_name": "",
+    "kr_link": "",
+    "jp_name": "",
+    "jp_link": "",
+    "na_name": "",
+    "na_link": "",
+}
+
 
 def open_browser():
     chrome_options = Options()
@@ -28,6 +32,8 @@ def open_browser():
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--headless")
+    # 창 안닫는 옵션
+    # chrome_options.add_experimental_option("detach", True)
     browser = webdriver.Chrome(
         executable_path=str(os.getenv("CHROMEDRIVER_PATH")),
         chrome_options=chrome_options,
@@ -50,6 +56,7 @@ def search_jp_db(keyword):
     jp_db_item_link = FFXIV_JP_URL + jp_db_item_link
     return jp_db_item_link
 
+
 def search_na_db(keyword):
     na_db_item_link = (
         get_soup(FFXIV_NA_URL + FFXIV_LS_DB + keyword)
@@ -59,22 +66,24 @@ def search_na_db(keyword):
     na_db_item_link = FFXIV_NA_URL + na_db_item_link
     return na_db_item_link
 
+
 def search_tarto(keyword):
     # TODO: 브라우저를 열고 닫지않고 그냥 계속 켜두는 방법
     browser = open_browser()
     browser.get(TARTO_URL + keyword)
-    
+
     try:
         item_link = browser.find_element_by_css_selector(
             "div.item-name > a"
         ).get_attribute("href")
+        item_search_result["kr_link"] = item_link
         browser.get(item_link)
     except:
         message = "검색결과가 없습니다."
 
     try:
         # 아이템 한국어명 취득
-        item_name = browser.find_element_by_css_selector(
+        item_search_result["kr_name"] = browser.find_element_by_css_selector(
             "div[id^='item-name'] > span"
         ).get_attribute("innerHTML")
 
@@ -82,13 +91,18 @@ def search_tarto(keyword):
         item_name_lang = browser.find_elements_by_css_selector(
             "div[id^='item-name-lang'] > span"
         )
-        item_name_na = item_name_lang[0].get_attribute("innerHTML")
-        item_name_jp = item_name_lang[2].get_attribute("innerHTML")
-        jp_link = search_jp_db(item_name_jp)
-        na_link = search_na_db(item_name_na)
-        message = f'→"{keyword}"의 검색결과 : \n\n・{item_name}\n{item_link}\n\n・{item_name_jp}\n{jp_link}\n\n・{item_name_na}\n・{na_link}'
+        item_search_result["ja_name"] = item_name_lang[2].get_attribute("innerHTML")
+        item_search_result["na_name"] = item_name_lang[0].get_attribute("innerHTML")
+        item_search_result["jp_link"] = search_jp_db(item_search_result["ja_name"])
+        item_search_result["na_link"] = search_na_db(item_search_result["na_name"])
+        return
     except:
         message = "오류가 발생했습니다."
 
-    browser.close()
+
+def result_message(items):
+    message = f'→"{items}"의 검색결과 : \n\n・{item_search_result["kr_name"]}\n{item_search_result["kr_link"]}\n\n・{item_search_result["ja_name"]}\n{item_search_result["jp_link"]}\n\n・{item_search_result["na_name"]}\n・{item_search_result["na_link"]}'
+    return message
+
+    # browser.close()
     return message
