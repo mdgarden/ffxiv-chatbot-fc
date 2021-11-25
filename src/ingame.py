f3611c4933375bead4d7a98b72376eb76a8edb40
Madang Garden <66259746+mdgarden@users.noreply.github.com>
2021-11-25 22:10:58 +0900

    에오르제아 시간 계산방법 조사

--- src/ingame.py
+++ src/ingame.py
@@ -1,1 +1,11 @@
 # TODO: get Erozea time / Server time
+
+
+def get_eorze_time():
+    # get current time
+    # 계산식 넣기
+    # /* Convert to Eorzea time */
+    # /* In Eorzea, 12 game minutes = 35 real seconds */
+    # if (eMin) *eMin = (currentTime * 12 / 35) % 60;
+    # if (eHour) *eHour = (currentTime * 12 / 35 / 60) % 24;
+    pass
